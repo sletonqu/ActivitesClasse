66d28c7d75fe06e66db4c158c70d455dd3bc4a0c
ILLICO <illico@laposte.net>
2026-03-31 22:16:23 +0200

    Gestion de la db en volume persistant

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const fs = require('fs');
 
-const dbPath = path.join(__dirname, 'database.sqlite');
+const dbPath = process.env.DB_PATH || path.join(__dirname, 'database.sqlite');
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error('Erreur de connexion à SQLite:', err.message);
